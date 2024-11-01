846b4bea48e22d0e3a6a87a61a060e07b0bd29ed
Charanvattikuti <122088261+Charanvattikuti@users.noreply.github.com>
2024-11-01 22:57:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,32 +26,37 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
+// Set the date we're counting down to
 var countDownDate = new Date("Nov 20, 2024 09:37:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get todays date and time
+    // Get today's date and time
     var now = new Date().getTime();
     
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
+    // Time calculations for days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    // Display the results in the elements with respective IDs
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
     
-    // If the count down is over, write some text 
+    // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("countdown-timer").innerHTML = "Bless the married couple for a happy life!";
     }
 }, 1000);
+
 
 // being a bit cool :p  
 var styles = [
