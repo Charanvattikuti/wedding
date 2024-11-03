d8d926847c6ca5ae88bf0967df0b719a17d0b5a9
Charanvattikuti <122088261+Charanvattikuti@users.noreply.github.com>
2024-11-03 18:31:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -98,3 +98,74 @@ console.log(
     `%cShaadi me zaroor aana!\n\n`,
     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
 )
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '1.5rem',
+    duration: 2000,
+    delay: 200,
+    // reset: true
+});
+
+
+// sr.reveal(`.rahul`,{ delay: 300, origin: 'top' })
+sr.reveal(`.rahul`, { origin: 'left', delay: 700 })
+sr.reveal(`.pranjal`, { origin: 'right', delay: 700 })
+sr.reveal(`.gif,.home_txt`, { delay: 1300, origin: 'top' })
+sr.reveal(`.top-right-decoration,.top-left-decoration`, { origin: 'left', delay: 2200 })
+sr.reveal(`.responsive-table__row`, { interval: 200 })
+sr.reveal(`.table_container`, { delay: 100, origin: 'top' })
+sr.reveal(`.happiness,.hindi_text`, { delay: 400, origin: 'bottom' })
+sr.reveal(`.social__links`, { delay: 1000, origin: 'bottom', interval: 200 })
+sr.reveal(`.newsletter__description,.home__bottom`, { origin: 'left', delay: 400 })
+sr.reveal(`.newsletter__form,.CTA`, { origin: 'right', delay: 400 })
+
+
+
+
+
+
+
+
+
+// being a bit cool :p  
+var styles = [
+    'background: linear-gradient(#D33106, #571402)'
+    , 'border: 4px solid #3E0E02'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
+    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+var styles1 = [
+    'color: #FF6C37'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+    , 'line-height: 40px'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+var styles2 = [
+    'color: teal'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+    , 'line-height: 40px'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+console.log('\n\n%c SAVE THE DATE: Dec 04, 2023!', styles);
+
+console.log('%cYour presence is requested!%c\n\nRegards: Rahul Jangir', styles1, styles2);
+
+console.log(
+    `%cShaadi me zaroor aana!\n\n`,
+    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
+)
